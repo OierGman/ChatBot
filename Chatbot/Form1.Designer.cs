24d79c39d82fd9c2605aa061991e91cb6cd4c234
jpinn97 <JP878@canterbury.ac.uk>
2022-11-07 15:28:28 +0000

    Chatbox UI - message log system

--- Chatbot/Form1.Designer.cs
+++ Chatbot/Form1.Designer.cs
@@ -29,24 +29,74 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.userInputBox = new System.Windows.Forms.TextBox();
+            this.messageButton = new System.Windows.Forms.Button();
+            this.chatLogTable = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
+            // 
+            // userInputBox
+            // 
+            this.userInputBox.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.userInputBox.Location = new System.Drawing.Point(12, 456);
+            this.userInputBox.Multiline = true;
+            this.userInputBox.Name = "userInputBox";
+            this.userInputBox.Size = new System.Drawing.Size(420, 132);
+            this.userInputBox.TabIndex = 1;
+            this.userInputBox.Text = "Say something";
+            this.userInputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.userInputBox_KeyPress);
+            // 
+            // messageButton
+            // 
+            this.messageButton.Location = new System.Drawing.Point(438, 456);
+            this.messageButton.Name = "messageButton";
+            this.messageButton.Size = new System.Drawing.Size(99, 132);
+            this.messageButton.TabIndex = 2;
+            this.messageButton.Text = "Send Message";
+            this.messageButton.UseVisualStyleBackColor = true;
+            this.messageButton.Click += new System.EventHandler(this.messageButton_Click);
+            // 
+            // chatLogTable
+            // 
+            this.chatLogTable.ColumnCount = 2;
+            this.chatLogTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.chatLogTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.chatLogTable.Location = new System.Drawing.Point(12, 12);
+            this.chatLogTable.Name = "chatLogTable";
+            this.chatLogTable.RowCount = 4;
+            this.chatLogTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.chatLogTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.chatLogTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.chatLogTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.chatLogTable.Size = new System.Drawing.Size(525, 438);
+            this.chatLogTable.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(549, 600);
+            this.Controls.Add(this.chatLogTable);
+            this.Controls.Add(this.messageButton);
+            this.Controls.Add(this.userInputBox);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(567, 647);
+            this.MinimumSize = new System.Drawing.Size(567, 647);
             this.Name = "Form1";
             this.Text = "Chatty";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox userInputBox;
+        private Button messageButton;
+        private TableLayoutPanel chatLogTable;
     }
 }
--- Chatbot/Form1.cs
+++ Chatbot/Form1.cs
@@ -2,9 +2,115 @@ namespace Chatbot
 {
     public partial class Form1 : Form
     {
+        private int _messageNumber = 2;
+
         public Form1()
         {
             InitializeComponent();
+            // initialize with welcome chatbot message
+            chatLogTable.Controls.Add(new TextBox()
+            {
+                ReadOnly = true,
+                Dock = DockStyle.Fill,
+                Multiline = true,
+                Text = "Hello! I'm Chatty, your personal assistant! How can I help?"
+            }, 0, 3);
+        }
+        // user message button click event
+        private void messageButton_Click(object sender, EventArgs e)
+        {
+            TextBox message = new TextBox()
+            {
+                ReadOnly = true,
+                Dock = DockStyle.Fill,
+                Multiline = true,
+                Text = userInputBox.Text,
+            };
+            userInputBox.Text = "";
+            ChatLogController(message);
+        }
+
+        private void ChatLogController(TextBox message)
+        {
+            var botMessageOne = chatLogTable.GetControlFromPosition(0, 3);
+            var botMessageTwo = chatLogTable.GetControlFromPosition(0, 2);
+            var botMessageThree = chatLogTable.GetControlFromPosition(0, 1);
+            chatLogTable.Controls.Remove(chatLogTable.GetControlFromPosition(0,0));
+            var userMessageOne = chatLogTable.GetControlFromPosition(1, 3);
+            var userMessageTwo = chatLogTable.GetControlFromPosition(1, 2);
+            var userMessageThree = chatLogTable.GetControlFromPosition(1, 1);
+            chatLogTable.Controls.Remove(chatLogTable.GetControlFromPosition(1, 0));
+            // no 1 2, 0 3, 0 2, 0 0
+            chatLogTable.Controls.Clear();
+            try
+            {
+                chatLogTable.Controls.Add(botMessageOne, 0, 2);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                chatLogTable.Controls.Add(botMessageTwo, 0, 1);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                chatLogTable.Controls.Add(botMessageThree, 0, 0);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                chatLogTable.Controls.Add(userMessageOne, 1, 2);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                chatLogTable.Controls.Add(userMessageTwo, 1,1);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                chatLogTable.Controls.Add(userMessageThree, 1, 0);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            chatLogTable.Controls.Add(message, 1, 3);
+        }
+
+        private void userInputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != (char)Keys.Enter) // guard statement to return early
+            {
+                return;
+            }
+            TextBox message = new TextBox()
+            {
+                ReadOnly = true,
+                Dock = DockStyle.Fill,
+                Multiline = true,
+                Text = userInputBox.Text,
+            };
+            userInputBox.Text = "";
+            ChatLogController(message);
         }
     }
 }

a31da8c797f3ed59336d5a68843f41d1ec5ad03e
jpinn97 <JP878@canterbury.ac.uk>
2022-11-08 22:02:45 +0000

    YouTube Music API.

--- Chatbot/ChatBotEngine.cs
+++ Chatbot/ChatBotEngine.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
 
 namespace Chatbot
 {
@@ -17,6 +19,35 @@ namespace Chatbot
             string responseBody = await response.Content.ReadAsStringAsync();
 
             APIObjects.BankHolidays.Root myDeserializedClass = JsonSerializer.Deserialize<APIObjects.BankHolidays.Root>(responseBody);
+        }
+        private async Task YouTubeMusic(string keyWord)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyC_oVYtu9mGyEaNT42rylHayaVWcFyez0c",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = keyWord; // Replace with your search term.
+            searchListRequest.MaxResults = 1;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+                }
+            }
         }
     }
 }
